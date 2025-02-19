fae241b22372d19633970d4c0e2b8fbf17752187
Daya Juang Mahaputra <dayajuangmahaputra@gmail.com>
2025-02-19 14:11:21 +0900

    プロジェクト ファイルを追加します。

---
+++ ConsoleApp/ConsoleApp.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int numbers[] = { 1,2,3 };
+	int* num_ptr = numbers;
+
+	cout << num_ptr << endl;
+	cout << num_ptr + 1 << endl;
+	cout << num_ptr + 2 << endl;
+
+	return 0;
+}
