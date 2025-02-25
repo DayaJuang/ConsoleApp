1025df7d3da5bc04cda88db426bdcc910b7c0fd6
Daya Juang Mahaputra <dayajuangmahaputra@gmail.com>
2025-02-25 12:55:06 +0900

    [add] Create an object

--- ConsoleApp/ConsoleApp.cpp
+++ ConsoleApp/ConsoleApp.cpp
@@ -3,11 +3,12 @@
 using namespace std;
 
 class Player {
-	
+public:
 	// attribute
 	string name;
 	int health;
 
+public:
 	// method
 	void talk(string dialogue);
 	void is_dead();
@@ -20,6 +21,10 @@ int main()
 
 	Player* enemy = nullptr;
 	enemy = new Player();
+
+	enemy->name = "Slime";
+	enemy->health = 100;
+
 
 	delete enemy;
 
